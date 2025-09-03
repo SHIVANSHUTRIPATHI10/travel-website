9cd28e073b890ea308bb24f8f96d6c9593253218
SHIVANSHUTRIPATHI10 <shivanshutripathi03@gmail.com>
2025-09-03 23:16:56 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,1 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-# Load sample datase
-tips= sns.load_dataset("tips")
-sns.pairplot(tips,vars=["total_bill","tip","size"],hue="sex")
-plt.show()
